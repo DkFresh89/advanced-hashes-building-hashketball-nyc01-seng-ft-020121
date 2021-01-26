b47efb3c80686b03cce11dcd555ff8349231a175
Doug K <doug.kerzner@gmail.com>
2021-01-26 16:10:07 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -68,7 +68,7 @@ def game_hash
       colors: ["Turquoise", "Purple"],
       players: [
          {
-           player_name: "Jeff Aidrien",
+           player_name: "Jeff Adrien",
         number: 4,
           shoe: 18,
           points: 10,
